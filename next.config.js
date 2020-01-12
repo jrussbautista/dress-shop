1f8945f4bf893f935b8bbef72ca35b59d22a2ce1
dodeca-dev <jrussclay@gmail.com>
2020-01-12 11:25:06 +0800

    gitignore next.config.js

--- next.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  env: {
-    MONGO_URL:
-      "mongodb+srv://russ:5raJsiiABptRZpxx@cluster0-654rk.mongodb.net/dress-shop?retryWrites=true&w=majority",
-    JWT_SECRET: "mysecretkey"
-  }
-};

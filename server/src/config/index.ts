c0b17d6534b2f48728a22f025adaf9b12357659e
jrussumbrella <jrussclay@gmail.com>
2021-06-10 23:33:40 +0800

    changed database uri constanst value

--- server/src/config/index.ts
+++ server/src/config/index.ts
@@ -1,10 +1,8 @@
-export const IS_PROD = process.env.NODE_ENV === "production";
+export const IS_PROD = process.env.NODE_ENV === 'production';
 export const PORT = process.env.PORT || 5000;
-export const DATABASE_URI = IS_PROD
-  ? process.env.DATABASE_URI_PROD
-  : process.env.DATABASE_URI_LOCAL;
+export const DATABASE_URI = process.env.DATABASE_URI;
 export const JWT_SECRET_KEY = `${process.env.JWT_SECRET}`;
-export const JWT_EXPIRES_IN = "7d";
+export const JWT_EXPIRES_IN = '7d';
 export const STRIPE_SECRET_KEY = `${process.env.STRIPE_SECRET_API_KEY}`;
 export const GOOGLE_WEB_CLIENT_ID = `${process.env.GOOGLE_WEB_CLIENT_ID}`;
 export const GOOGLE_ANDROID_CLIENT_ID = `${process.env.GOOGLE_ANDROID_CLIENT_ID}`;

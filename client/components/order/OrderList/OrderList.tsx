eab551cb272e9d722cc57da3e450c6dd3f92812a
jrussumbrella <jrussclay@gmail.com>
2021-08-24 23:40:45 +0800

    Refactor order list

--- client/components/order/OrderList/OrderList.tsx
+++ client/components/order/OrderList/OrderList.tsx
@@ -34,39 +34,35 @@ const OrderList = () => {
                   <span className={styles.dateText}>{formatDate(order.createdAt)}</span>
                 </div>
                 <div className="products">
-                  {orders.map((order) => (
-                    <>
-                      {order.items.map((product) => (
-                        <div key={product.product._id} className={styles.productList}>
-                          <div className={styles.productWrapper}>
-                            <Link href={`/product?id=${product.product._id}`}>
+                  {order.items.map((order) => (
+                    <div key={order.product._id} className={styles.productList}>
+                      <div className={styles.productWrapper}>
+                        <Link href={`/products/${order.product._id}`}>
+                          <a>
+                            <Image
+                              src={order.product.imageURL}
+                              alt={order.product.name}
+                              width={150}
+                              height={150}
+                            />
+                          </a>
+                        </Link>
+
+                        <div className={styles.productInfo}>
+                          <div>
+                            <Link href={`/products/${order.product._id}`}>
                               <a>
-                                <Image
-                                  src={product.product.imageURL}
-                                  alt={product.product.name}
-                                  width={150}
-                                  height={150}
-                                />
+                                <div className={styles.productName}>{order.product.name}</div>
                               </a>
                             </Link>
-
-                            <div className={styles.productInfo}>
-                              <div>
-                                <Link href={`/product?id=${product.product._id}`}>
-                                  <a>
-                                    <div className={styles.productName}>{product.product.name}</div>
-                                  </a>
-                                </Link>
-                                <div className={styles.productQuantity}>x {product.quantity}</div>
-                              </div>
-                              <div className={styles.productPrice}>
-                                {formatPrice(product.product.price * product.quantity)}
-                              </div>
-                            </div>
+                            <div className={styles.productQuantity}>x {order.quantity}</div>
+                          </div>
+                          <div className={styles.productPrice}>
+                            {formatPrice(order.product.price * order.quantity)}
                           </div>
                         </div>
-                      ))}
-                    </>
+                      </div>
+                    </div>
                   ))}
                 </div>
                 <div className={styles.orderBottom}>

c0b028dfb83d654d2342546e701a218164dfef24
James Russel C. Bautista <jrussclay@gmail.com>
2022-10-16 18:58:58 +0800

    Update login tests

--- client/components/auth/LoginForm/LoginForm.test.tsx
+++ client/components/auth/LoginForm/LoginForm.test.tsx
@@ -15,35 +15,31 @@ jest.mock('next/router', () => ({
   },
 }));
 
-test('successfully login user and navigated to profile page', async () => {
-  render(<LoginForm />);
-  const fields = userGenerator();
+describe('<LoginForm />', () => {
+  test('successfully login user and navigated to profile page', async () => {
+    render(<LoginForm />);
+    const fields = userGenerator();
+    const emailInput = screen.getByPlaceholderText(/email/i);
+    const passwordInput = screen.getByPlaceholderText(/password/i);
 
-  const emailInput = screen.getByPlaceholderText(/email/i);
-  const passwordInput = screen.getByPlaceholderText(/password/i);
+    userEvent.type(emailInput, fields.email);
+    userEvent.type(passwordInput, fields.password);
 
-  userEvent.type(emailInput, fields.email);
-  userEvent.type(passwordInput, fields.password);
+    const submitButton = screen.getByRole('button', { name: /log in/i });
+    await waitFor(() => userEvent.click(submitButton));
 
-  const submitButton = screen.getByRole('button', { name: /log in/i });
+    expect(submitButton).toBeDisabled();
+    await waitFor(() => expect(submitButton).toBeEnabled());
+    expect(Router.push).toBeCalledWith('/profile');
+  });
 
-  await waitFor(() => userEvent.click(submitButton));
+  test('display input fields error when input fields is empty on form submit', async () => {
+    render(<LoginForm />);
+    const submitButton = screen.getByRole('button', { name: /log in/i });
 
-  expect(submitButton).toBeDisabled();
+    userEvent.click(submitButton);
+    await waitFor(() => expect(screen.getByText('Email is required')).toBeInTheDocument());
 
-  await waitFor(() => expect(submitButton).toBeEnabled());
-
-  expect(Router.push).toBeCalledWith('/profile');
-});
-
-test('display input fields error when input fields is empty on form submit', async () => {
-  render(<LoginForm />);
-
-  const submitButton = screen.getByRole('button', { name: /log in/i });
-
-  userEvent.click(submitButton);
-
-  await waitFor(() => expect(screen.getByText('Email is required')).toBeInTheDocument());
-
-  expect(screen.getByText('Password is required')).toBeInTheDocument();
+    expect(screen.getByText('Password is required')).toBeInTheDocument();
+  });
 });
